feeef545137216e03ffa23a2a09ed13ada4c50bc
0-EWT-0 <157067700+0-EWT-0@users.noreply.github.com>
2025-05-22 19:09:37 -0400

    Cuadritos feos

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,47 @@
 import Header from "./components/Header.component.vue";
 </script>
 
+aca va ir el panel de dashboard
 <template>
   <Header class="bg-black" />
+
+  <!--Esto ponerlo en utils-->
+  <div class="cuadritos grid grid-cols-2 gap-4">
+    <div class="bg-green h-60 w-40 rounded-lg justify-center">
+      <h1 class="mb-6">Etapa 1</h1>
+      <b
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        egestas justo ut malesuada sollicitudin. Vestibulum felis nunc,
+        imperdiet sed odio placerat, ullamcorper rutrum justo</b
+      >
+      <button class="py-2 px-4 bg-white">Continuar</button>
+    </div>
+    <div class="bg-blue h-60 w-40 rounded-lg">
+      <h1 class="mb-6">Etapa 1</h1>
+      <b
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        egestas justo ut malesuada sollicitudin. Vestibulum felis nunc,
+        imperdiet sed odio placerat, ullamcorper rutrum justo</b
+      >
+      <button class="py-2 px-4 bg-white">Continuar</button>
+    </div>
+    <div class="bg-orange h-60 w-40 rounded-lg">
+      <h1 class="mb-6">Etapa 1</h1>
+      <b
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        egestas justo ut malesuada sollicitudin. Vestibulum felis nunc,
+        imperdiet sed odio placerat, ullamcorper rutrum justo</b
+      >
+      <button class="py-2 px-4 bg-white">Continuar</button>
+    </div>
+    <div class="bg-red h-60 w-50 rounded-lg">
+      <h1 class="mb-6">Etapa 1</h1>
+      <b
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        egestas justo ut malesuada sollicitudin. Vestibulum felis nunc,
+        imperdiet sed odio placerat, ullamcorper rutrum justo</b
+      >
+      <button class="py-2 px-4 bg-white">Continuar</button>
+    </div>
+  </div>
 </template>
