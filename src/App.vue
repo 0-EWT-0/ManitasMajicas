569d7b41b3116e4333c9b99ed3d9f6e180b1d598
0-EWT-0 <157067700+0-EWT-0@users.noreply.github.com>
2025-05-22 18:46:00 -0400

    componente header

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Header from "./components/Header.component.vue";
+</script>
+
+<template>
+  <Header class="bg-black" />
+</template>
---
+++ src/components/Header.component.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="header bg-white justify-between py-14 shadow-md">
+    <div class="justify-center flex grid-cols-3 gap-6 bg-white">
+      <h1>Icono</h1>
+      <h1>Icono</h1>
+      <h1>Icono</h1>
+    </div>
+  </div>
+</template>
